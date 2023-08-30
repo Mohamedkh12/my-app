15425c37af1eb478aa6f380629c07b949a7190d9
Mohamedkhalilkhelifi <mohamedkhalilkhelifi69@gmail.com>
2023-08-30 11:24:16 +0100

    changer le contenu du fichier

--- src/Specials.js
+++ src/Specials.js
@@ -1,35 +1,69 @@
 import "./Specials.css";
-import salad from "./icons_assets/greek salad.jpg"
-import Burchetta from "./icons_assets/bruchetta.svg"
-import dessert from "./icons_assets/lemon dessert.jpg"
-export function Specials(){
-    return(
-        <div className="special">
-            <h1>SPECIALS</h1>
-            <button>Online Menu</button>
-            <div className="card">
-                <image src={salad}></image>
-                <h3>Greek Salad</h3>
-                <p>$12.99</p>
-                <p> Diam quam nulla porttitor massa. Sit amet aliquam id diam. Ut venenatis tellus in metus. Fringilla phasellu</p>
-                <p>Order a delevery</p>
-            </div>
-            <div className="card">
-                <image src={Burchetta}></image>
-                <h3>Burchetta</h3>
-                <p>$5.99</p>
-                <p> Diam quam nulla porttitor massa. Sit amet aliquam id diam. Ut venenatis tellus in metus. Fringilla phasellu</p>
-                <p>Order a delevery</p>
-            </div>
-            <div className="card">
-                <image src={dessert}></image>
-                <h3>lemon Dessert</h3>
-                <p>$5.00</p>
-                <p> Diam quam nulla porttitor massa. Sit amet aliquam id diam. Ut venenatis tellus in metus. Fringilla phasellu</p>
-                <p>Order a delevery</p>
-            </div>
+import salad from "./icons_assets/greek salad.jpg";
+import Burchetta from "./icons_assets/bruchetta.svg";
+import dessert from "./icons_assets/lemon dessert.jpg";
 
 
-        </div>
-    )
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
+export function Specials() {
+  return (
+    <CardGroup className="core">
+      <Card>
+        <Card.Img variant="top" src={salad} />
+        <Card.Body>
+        <div className="card-price">
+          <Card.Title>Greek Salad</Card.Title>
+          
+          <Card.Text className="price">$12.99</Card.Text>
+          </div>
+          <Card.Text>
+          Diam quam nulla porttitor massa. Sit amet aliquam id diam. Ut
+            venenatis tellus in metus. Fringilla phasellu
+          </Card.Text>
+        
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Order a delevery</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={Burchetta} />
+        <Card.Body>
+        <div className="card-price">
+          <Card.Title>Burchetta</Card.Title>
+          <Card.Text className="price"> $5.99</Card.Text>
+          </div>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Order a delevery</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={dessert} />
+        <Card.Body>
+        <div className="card-price">
+          <Card.Title>lemon Dessert</Card.Title>
+          <Card.Text className="price"> $5.00</Card.Text>
+          </div>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+  );
 }
+
+export default Specials;
